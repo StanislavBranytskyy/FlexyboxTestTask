95a25493446004cc3c38d1c4413b3ac38617a91b
Stanislav Branytskyy <Stanislav_Branytskyy@epam.com>
2017-01-09 00:52:42 +0200

    refactoring

--- FlexyboxTestTask/InstanceService.cs
+++ FlexyboxTestTask/InstanceService.cs
@@ -5,15 +5,22 @@ using System.Linq;
 
 namespace FlexyboxTestTask
 {
-    public static class InstanceService<T> 
+    public sealed class InstanceService<T>
     {
+        private static IEnumerable<T> _instances;
+
         public static IEnumerable<T> GetInstances()
         {
-            var assembly = typeof(T).Assembly;
+            if (_instances == null)
+            {
+                var assembly = typeof(T).Assembly;
 
-            var types = assembly.GetTypes().Where(t => t.BaseType == typeof(T)).ToList();
+                var types = assembly.GetTypes().Where(t => t.BaseType == typeof(T)).ToList();
 
-            return types.Select(t => (T) Activator.CreateInstance(t)).ToList();
+                _instances = types.Select(t => (T)Activator.CreateInstance(t)).ToList();
+            }
+
+            return _instances;
         }
     }
 }
